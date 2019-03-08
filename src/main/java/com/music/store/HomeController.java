2a7f6ac1178325480039682339f85029739868da
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-03-09 00:12:58 +0300

    initial commit

---
+++ src/main/java/com/music/store/HomeController.java
@@ -0,0 +1,16 @@
+package com.music.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Dmitrii on 09.03.2019.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to the Music Store</title>
+</head>
+<body>
+<h1>Welcome to the Music Store</h1>
+
+</body>
+</html>
