58c68c07b1c781ded309ed59a2569e471b928d5a
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-03-10 00:03:16 +0300

    stub dao implementation for the product list page

---
+++ src/main/java/com/music/store/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.music.store.controller;
+
+import com.music.store.dao.ProductDao;
+import com.music.store.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Created by Dmitrii on 09.03.2019.
+ */
+@Controller
+public class HomeController {
+
+    private ProductDao productDao = new ProductDao();
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model) {
+        List<Product> productList = productDao.getProductList();
+        model.addAttribute("products", productList);
+
+        return "productList";
+    }
+
+}
