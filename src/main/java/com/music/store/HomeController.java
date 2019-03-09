3c97204310d20567dcb9d948c408596bcff6f88b
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-03-09 23:05:40 +0300

    Linked frontend and backend

--- src/main/java/com/music/store/HomeController.java
+++ src/main/java/com/music/store/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/")
     public String getHomePage() {
         return "index";
     }
