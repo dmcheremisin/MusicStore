4b133d10317833c9f7a56097b9c62f8eb91daacc
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-03-26 00:01:53 +0300

    product list

--- src/main/java/com/music/store/controller/ProductController.java
+++ src/main/java/com/music/store/controller/ProductController.java
@@ -1,17 +1,13 @@
 package com.music.store.controller;
 
 import com.music.store.converters.ProductToProductViewModelConverter;
-import com.music.store.dao.ProductDao;
 import com.music.store.entity.Product;
 import com.music.store.services.ProductService;
-import com.music.store.viewModels.ProductViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -26,13 +22,9 @@ public class ProductController {
     private ProductService productService;
 
     @RequestMapping("productList")
-    public String getProducts(@RequestParam(value = "searchCondition", required = false) String searchCondition, Model model) {
+    public String getProducts(Model model) {
         List<Product> productViewModels = productService.getProductList();
         model.addAttribute("products", productViewModels);
-        if(!StringUtils.isEmpty(searchCondition)) {
-            model.addAttribute("searchCondition", searchCondition);
-        }
-
         return "productList";
     }
 
